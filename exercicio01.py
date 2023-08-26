3f519c2c0cf42f26191d6d66fc4646a21ecc9454
DevJulioLima <julioempresarioexecultivo@gmail.com>
2023-08-26 14:09:44 -0300

    primeiro exercicio em python

---
+++ exercicio01.py
@@ -0,0 +1,13 @@
+nome = str("julio")
+sobrenome = str("lima")
+idade = int(26)
+ano_nascimento = int(1997)
+altura_metros = float(1.79)
+maior_de_idade = idade >= 18
+
+print("nome:", nome)
+print("sobrenome:", sobrenome)
+print("idade:", idade)
+print("ano de nascimento:", ano_nascimento)
+print("é maior de idade?", maior_de_idade)
+print("altura em metros:", altura_metros)
